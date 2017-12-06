ffaa5573c80047febe6b174a30533638961bd841
Amar Chatman <amarjerome@gmail.com>
2017-12-06 22:18:29 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    puts "O"
+  else
+    puts "X"
+end
+end 
